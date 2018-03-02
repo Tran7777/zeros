8edd1a753280de02083cc6978c2a63ac11ea1ea5
Tran7777 <30432795+Tran7777@users.noreply.github.com>
2018-03-02 14:07:52 +0000

    Update index.js
    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    let temp =  0;
+    let res = 0;
+  
+    for (let index = 5; index <= 9765625; index*5) {
+        
+        temp = Math.floor(number / index);
+        res += temp;
+    }
+    if (number >= 48828125) {
+        temp = Math.floor(number / 48828125);
+        res += temp;
+    }
+    return(res);
+   
 }
